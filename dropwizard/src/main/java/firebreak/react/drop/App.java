b8ee2db35757f529c70eb41935655a534f38c32f
Kasun Kumara <kasun.kumara@gmail.com>
2016-10-14 21:01:56 +0100

    add main config yaml

--- dropwizard/src/main/java/firebreak/react/drop/App.java
+++ dropwizard/src/main/java/firebreak/react/drop/App.java
@@ -1,9 +1,13 @@
 package firebreak.react.drop;
 
+import com.google.common.io.Resources;
 import firebreak.react.drop.resources.CardResource;
 import firebreak.react.drop.service.AuthorisationService;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
+
+import java.io.File;
+import java.net.URISyntaxException;
 
 public class App extends Application<AppConfiguration> {
 
@@ -15,8 +19,12 @@ public class App extends Application<AppConfiguration> {
 
     public static void main(String[] args) throws Exception {
         if (args == null || args.length == 0) {
-            args = new String[]{"server"};
+            args = new String[]{"server", resourceFilePath("config.yaml")};
         }
         new App().run(args);
+    }
+
+    private static String resourceFilePath(String relativePath) throws URISyntaxException {
+        return new File(Resources.getResource(relativePath).toURI()).getAbsolutePath();
     }
 }
